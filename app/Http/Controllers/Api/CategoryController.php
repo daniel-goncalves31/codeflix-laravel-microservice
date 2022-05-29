da5364660e43026b05c1c9647c5f0f3b08ca0e35
Daniel Gonçalves <danielgg0831@gmail.com>
2022-05-29 14:07:07 -0300

    feat(CategoryController): implement the update method

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -52,7 +52,8 @@ class CategoryController extends Controller
      */
     public function update(UpdateCategoryRequest $request, Category $category)
     {
-        //
+        $category->update($request->all());
+        return $category;
     }
 
     /**
