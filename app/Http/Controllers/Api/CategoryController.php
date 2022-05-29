1ab3621a474522142c9acbc062936127c694da65
Daniel Gonçalves <danielgg0831@gmail.com>
2022-05-29 14:09:36 -0300

    feat(CategoryController): implement the destroy method

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -64,6 +64,7 @@ class CategoryController extends Controller
      */
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return response()->noContent();
     }
 }
