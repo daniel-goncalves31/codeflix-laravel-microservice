81435ca0fbaed3991c099f540014ae2737d03e39
Daniel Gonçalves <danielgg0831@gmail.com>
2022-05-31 22:37:17 -0300

    test: create category unit test

---
+++ tests/Unit/Models/CategoryTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Category;
+use PHPUnit\Framework\TestCase;
+
+class CategoryTest extends TestCase
+{
+
+    public function test_fillable()
+    {
+        $fillable = ['name', 'description', 'is_active'];
+        $category = new Category();
+        $this->assertEquals($category->getFillable(), $fillable);
+    }
+
+    public function test_dates()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+        $category = new Category();
+        $this->assertEquals($category->getDates(), $dates);
+    }
+
+    public function test_keyType()
+    {
+        $keyType = 'string';
+        $category = new Category();
+        $this->assertEquals($category->getKeyType(), $keyType);
+    }
+
+    public function test_incrementing()
+    {
+        $category = new Category();
+        $this->assertFalse($category->getIncrementing());
+    }
+}
