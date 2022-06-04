df2baae388e50a168edd698448df7c1f7bc7e91c
Daniel Gonçalves <danielgg0831@gmail.com>
2022-06-04 22:26:18 +0000

    refactor: refresh category model on store method

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -28,7 +28,11 @@ class CategoryController extends Controller
      */
     public function store(StoreCategoryRequest $request)
     {
-        return Category::create($request->all());
+
+        $category = Category::create($request->all());
+        $category->refresh();
+
+        return $category;
     }
 
     /**
